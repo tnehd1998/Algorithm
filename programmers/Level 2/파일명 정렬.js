6b1529776c22193f0735640818fc36fec3df2bfa
이수동 <tnehd1998@gmail.com>
2022-02-27 20:34:48 +0900

    2022/02/27 알고리즘 문제풀이

---
+++ programmers/Level 2/파일명 정렬.js
@@ -0,0 +1,47 @@
+// 생각한 풀이 방법
+// 1. 숫자가 나오기 직전인 HEAD를 정규표현식으로 구한다.
+// 2. HEAD를 기준으로 정렬을 한다.
+// 3. HEAD가 같을 경우, NUMBER부분을 정규표현식으로 구한다.
+// 4. NUMBER의 크기에 따라 정렬을 한다.
+
+function solution(files) {
+  return files.sort((a, b) => {
+    // 숫자가 나오기 직전인 HEAD를 정규표현식으로 구한다.
+    let wordA = a.match(/^\D+/)[0].toLowerCase();
+    let wordB = b.match(/^\D+/)[0].toLowerCase();
+
+    // HEAD를 기준으로 정렬을 한다.
+    if (wordA < wordB) {
+      return -1;
+    }
+    if (wordA > wordB) {
+      return 1;
+    }
+
+    // HEAD가 같을 경우, NUMBER부분을 정규표현식으로 구한다.
+    let numberA = Number(a.match(/\d+/)[0]);
+    let numberB = Number(b.match(/\d+/)[0]);
+
+    // NUMBER의 크기에 따라 정렬을 한다.
+    return numberA - numberB;
+  });
+}
+
+console.log(
+  solution([
+    "img12.png",
+    "img10.png",
+    "img02.png",
+    "img1.png",
+    "IMG01.GIF",
+    "img2.JPG",
+  ])
+);
+console.log(
+  solution([
+    "F-5 Freedom Fighter",
+    "B-50 Superfortress",
+    "A-10 Thunderbolt II",
+    "F-14 Tomcat",
+  ])
+);
