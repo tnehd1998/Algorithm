efaaabc9a6c2891fd9e45c8e7d131f585c3e9e15
이수동 <tnehd1998@gmail.com>
2022-05-04 21:33:18 +0900

    2022/05/04 알고리즘 문제풀이

--- programmers/Level 1/신규 아이디 추천.js
+++ programmers/Level 1/신규 아이디 추천.js
@@ -1,27 +1,58 @@
+// function solution(new_id) {
+//   new_id = new_id.toLowerCase();
+
+//   let correct = /[^a-z0-9-_.]/g;
+//   new_id = new_id.replace(correct, "");
+
+//   let checkDot = /[.]+/g;
+//   new_id = new_id.replace(checkDot, ".");
+
+//   if (new_id[0] === ".") {
+//     new_id = new_id.substring(1, new_id.length);
+//   }
+
+//   if (new_id.length === 0) {
+//     new_id += "a";
+//   }
+
+//   new_id = new_id.substring(0, 15);
+//   if (new_id[new_id.length - 1] === ".") {
+//     new_id = new_id.substring(0, new_id.length - 1);
+//   }
+
+//   while (new_id.length < 3) {
+//     new_id += new_id[new_id.length - 1];
+//   }
+
+//   return new_id;
+// }
+
 function solution(new_id) {
   new_id = new_id.toLowerCase();
 
-  let correct = /[^a-z0-9-_.]/g;
-  new_id = new_id.replace(correct, "");
+  new_id = new_id.replace(/[^a-z0-9-_.]/g, "");
 
-  let checkDot = /[.]+/g;
-  new_id = new_id.replace(checkDot, ".");
+  new_id = new_id.replace(/[.]+/g, ".");
 
+  if (new_id[new_id.length - 1] === ".") {
+    new_id = new_id.slice(0, new_id.length - 1);
+  }
   if (new_id[0] === ".") {
-    new_id = new_id.substring(1, new_id.length);
+    new_id = new_id.slice(1, new_id.length);
   }
 
   if (new_id.length === 0) {
-    new_id += "a";
+    new_id = "a";
   }
 
-  new_id = new_id.substring(0, 15);
+  new_id = new_id.slice(0, 15);
   if (new_id[new_id.length - 1] === ".") {
-    new_id = new_id.substring(0, new_id.length - 1);
+    new_id = new_id.slice(0, new_id.length - 1);
   }
 
   while (new_id.length < 3) {
-    new_id += new_id[new_id.length - 1];
+    let word = new_id[new_id.length - 1];
+    new_id += word;
   }
 
   return new_id;
--- programmers/Level 2/메뉴 리뉴얼.js
+++ programmers/Level 2/메뉴 리뉴얼.js
@@ -4,16 +4,60 @@
 // 3. 해당 course에 따라 문자열 길이 중 최대값을 res에 push
 // 4. res을 정렬 후 반환
 
-function solution(orders, course) {
-  let menu = new Map();
+// function solution(orders, course) {
+//   let menu = new Map();
 
-  // 모든 조합을 확인해주는 함수
+//   // 모든 조합을 확인해주는 함수
+//   function combination(order, idx, len, prev) {
+//     if (prev.length === len) {
+//       let cur_key = prev.sort().join("");
+//       if (menu.has(cur_key)) {
+//         menu.set(cur_key, menu.get(cur_key) + 1);
+//       } else menu.set(cur_key, 1);
+//       return;
+//     }
+
+//     for (let i = idx; i < order.length; i++) {
+//       combination(order, i + 1, len, [...prev, order[i]]);
+//     }
+//   }
+
+//   // 나올수 있는 모든 경우를 menu에 추가, 존재 시 해당 값 증가
+//   orders.map((order) => {
+//     course.map((num) => combination(order, 0, num, []));
+//   });
+
+//   // 해당 값들중 2번 이상 나온것들만 filter하여 정렬
+//   menu = [...menu.entries()]
+//     .filter((item) => item[1] > 1)
+//     .sort((a, b) => b[0].length - a[0].length);
+
+//   let res = [];
+//   course.map((num) => {
+//     // 각 course에 따라 최대 값을 res에 push
+//     let max = 0;
+//     const tmp = menu.filter(([k, v]) => {
+//       if (max < v && k.length === num) max = v;
+//       return k.length === num;
+//     });
+//     tmp.filter((x) => x[1] === max).map((x) => res.push(x[0]));
+//   });
+
+//   return res.sort();
+// }
+
+function solution(orders, course) {
+  let allMenus = {};
+  let answer = [];
+
   function combination(order, idx, len, prev) {
     if (prev.length === len) {
-      let cur_key = prev.sort().join("");
-      if (menu.has(cur_key)) {
-        menu.set(cur_key, menu.get(cur_key) + 1);
-      } else menu.set(cur_key, 1);
+      let currentMenu = prev.sort().join("");
+      if (allMenus[currentMenu]) {
+        allMenus[currentMenu] += 1;
+      } else {
+        allMenus[currentMenu] = 1;
+      }
       return;
     }
 
@@ -22,28 +66,32 @@ function solution(orders, course) {
     }
   }
 
-  // 나올수 있는 모든 경우를 menu에 추가, 존재 시 해당 값 증가
-  orders.map((order) => {
-    course.map((num) => combination(order, 0, num, []));
-  });
+  for (let i = 0; i < orders.length; i++) {
+    for (let j = 0; j < course.length; j++) {
+      combination(orders[i], 0, course[j], []);
+    }
+  }
 
-  // 해당 값들중 2번 이상 나온것들만 filter하여 정렬
-  menu = [...menu.entries()]
-    .filter((item) => item[1] > 1)
-    .sort((a, b) => b[0].length - a[0].length);
+  for (let i = 0; i < course.length; i++) {
+    let maxValue = -1;
+    for (let menu in allMenus) {
+      if (menu.length === course[i]) {
+        if (allMenus[menu] > 1 && maxValue < allMenus[menu]) {
+          maxValue = allMenus[menu];
+        }
+      }
+    }
 
-  let res = [];
-  course.map((num) => {
-    // 각 course에 따라 최대 값을 res에 push
-    let max = 0;
-    const tmp = menu.filter(([k, v]) => {
-      if (max < v && k.length === num) max = v;
-      return k.length === num;
-    });
-    tmp.filter((x) => x[1] === max).map((x) => res.push(x[0]));
-  });
+    for (let menu in allMenus) {
+      if (menu.length === course[i]) {
+        if (maxValue === allMenus[menu]) {
+          answer.push(menu);
+        }
+      }
+    }
+  }
 
-  return res.sort();
+  return answer.sort();
 }
 
 console.log(
