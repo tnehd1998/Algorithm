589b55b1e4e8742eb277c5caffae4939f2f62a6b
이수동 <tnehd1998@gmail.com>
2022-06-07 17:00:24 +0900

    2022/06/07 알고리즘 문제풀이

---
+++ programmers/Level 2/멀리 뛰기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  const answer = [0, 1, 2];
+
+  for (let i = 3; i <= n; i++) {
+    answer[i] = (answer[i - 2] + answer[i - 1]) % 1234567;
+  }
+
+  return answer[n];
+}
+
+console.log(solution(4));
+console.log(solution(3));
